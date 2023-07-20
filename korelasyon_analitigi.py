acf88234e5d21e110843a093f01ee98aaa22d16e
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-07-20 23:17:52 +0300

    korelasyon analizi done

--- korelasyon_analitigi.py
+++ korelasyon_analitigi.py
@@ -14,14 +14,14 @@ num_cols = [col for col in df.columns if df[col].dtypes in [int, float]]
 corr = df[num_cols].corr()
 
 sns.set(rc={"figure.figsize": (12, 12)})
-sns.heatmap(
-    corr, annot=True, fmt=".2f", annot_kws={"size": 10}, linewidths=0.5, cmap="Blues"
-)
+sns.heatmap(corr, cmap="RdBu")
 plt.show()
 
-# Yüksek korelasyonlu degiskenlerin silinmesi
+############################################
+# Yüksek korelasyonlu değişkenlerden kurtulma
+############################################
 
-corr_matrix = df.corr().abs()
+corr_matrix = corr.abs()
 
 upper_triangle_matrix = corr_matrix.where(
     np.triu(np.ones(corr_matrix.shape), k=1).astype(bool)
@@ -35,20 +35,11 @@ drop_list = [
 ]
 
 corr_matrix[drop_list]
-
 df.drop(drop_list, axis=1)
 
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-
-file_path = r"C:\Users\Lenova\Desktop\MiuulYazKampı\data\data.csv"
-breast_cancer = pd.read_csv(file_path)
-df = breast_cancer.copy()
-df = df.iloc[:, 1:]
-
+############################################
+# Yüksek korelasyonlu değişkenlerden kurtulmak için fonksiyon
+############################################
 
 def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
     """
@@ -86,38 +77,9 @@ def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
     return drop_list
 
 
-num_cols = [col for col in df.columns if df[col].dtypes in [int, float]]
-
-corr = df[num_cols].corr()
-
-sns.set(rc={"figure.figsize": (12, 12)})
-sns.heatmap(
-    corr, annot=True, fmt=".2f", annot_kws={"size": 10}, linewidths=0.5, cmap="Blues"
-)
-plt.show()
-
-# Yüksek korelasyonlu degiskenlerin silinmesi
-
-corr_matrix = df.corr().abs()
-
-upper_triangle_matrix = corr_matrix.where(
-    np.triu(np.ones(corr_matrix.shape), k=1).astype(bool)
-)
-
-# yüzde doksandan büyük korelasyonu olan degiskenlerin isimlerini alalım
-drop_list = [
-    col
-    for col in upper_triangle_matrix.columns
-    if any(upper_triangle_matrix[col] > 0.90)
-]
-
-corr_matrix[drop_list]
-
-df.drop(drop_list, axis=1)
-
 high_correlated_cols(df)
 drop_list = high_correlated_cols(df, plot=True)
 
+df.drop(drop_list, axis=1)
+# korelasyon analizi yapılacak değişkenlerin seçilmesi
 drop_list = high_correlated_cols(df.drop(drop_list, axis=1), plot=True)
-
-
