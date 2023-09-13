b3d2b5d26c488d65d9bec05364917fa6ef8fdd5e
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-09-13 19:12:56 +0300

    log-reg theory done

--- machine_learning/linear_regresion.py
+++ machine_learning/linear_regresion.py
@@ -127,19 +127,89 @@ reg_model.score(X_test, y_test)
 # 10 katlı CV ile train hatası - 0.8959372632325174
 
 np.mean(
-    np.sqrt(
-        -cross_val_score(
-            reg_model,X, y, cv=10, scoring="neg_mean_squared_error"
-        )
-    )
+    np.sqrt(-cross_val_score(reg_model, X, y, cv=10, scoring="neg_mean_squared_error"))
 )
 
 # 5 katlı CV ile train hatası - 1.7175247278732086
 np.mean(
-    np.sqrt(
-        -cross_val_score(
-            reg_model,X, y, cv=5, scoring="neg_mean_squared_error"
+    np.sqrt(-cross_val_score(reg_model, X, y, cv=5, scoring="neg_mean_squared_error"))
+)
+
+#############################################
+# Simple Linear Regression Gradient Descent from Scratch
+#############################################
+
+
+# Cost function - MSE
+def cost_function(Y, b, w, X):
+    m = len(Y)
+    sse = 0
+
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        sse += (y_hat - y) ** 2
+
+    mse = sse / m
+    return mse
+
+
+# update_weights
+def update_weights(Y, b, w, X, learning_rate):
+    m = len(Y)
+    
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+    
+    for i in range(0, m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        
+        b_deriv_sum += y_hat - y
+        w_deriv_sum += (y_hat - y) * X[i]
+        
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+    return new_b, new_w
+
+
+# train fonksiyonu
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+    print(
+        "Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(
+            initial_b, initial_w, cost_function(Y, initial_b, initial_w, X)
         )
     )
-)
- 
+
+    b = initial_b
+    w = initial_w
+    cost_history = []
+
+    for i in range(num_iters):
+        b, w = update_weights(Y, b, w, X, learning_rate)
+        mse = cost_function(Y, b, w, X)
+        cost_history.append(mse)
+
+        if i % 100 == 0:
+            print("iter={:d}    b={:.2f}    w={:.4f}    mse={:.4}".format(i, b, w, mse))
+
+    print(
+        "After {0} iterations b = {1}, w = {2}, mse = {3}".format(
+            num_iters, b, w, cost_function(Y, b, w, X)
+        )
+    )
+    return cost_history, b, w
+
+
+df = advertising.copy()
+
+X = df["radio"]
+Y = df["sales"]
+
+# hyperparameters
+learning_rate = 0.001
+initial_b = 0.001
+initial_w = 0.001
+num_iters = 10000
+
+cost_history, b, w = train(Y, initial_b, initial_w, X, learning_rate, num_iters)
---
+++ machine_learning/logistic_regression.py
@@ -0,0 +1,50 @@
+# map of konya province
+######################################################
+# Diabetes Prediction with Logistic Regression
+######################################################
+
+# İş Problemi:
+
+# Özellikleri belirtildiğinde kişilerin diyabet hastası olup
+# olmadıklarını tahmin edebilecek bir makine öğrenmesi
+# modeli geliştirebilir misiniz?
+
+# Veri seti ABD'deki Ulusal Diyabet-Sindirim-Böbrek Hastalıkları Enstitüleri'nde tutulan büyük veri setinin
+# parçasıdır. ABD'deki Arizona Eyaleti'nin en büyük 5. şehri olan Phoenix şehrinde yaşayan 21 yaş ve üzerinde olan
+# Pima Indian kadınları üzerinde yapılan diyabet araştırması için kullanılan verilerdir. 768 gözlem ve 8 sayısal
+# bağımsız değişkenden oluşmaktadır. Hedef değişken "outcome" olarak belirtilmiş olup; 1 diyabet test sonucunun
+# pozitif oluşunu, 0 ise negatif oluşunu belirtmektedir.
+
+# Değişkenler
+# Pregnancies: Hamilelik sayısı
+# Glucose: Glikoz.
+# BloodPressure: Kan basıncı.
+# SkinThickness: Cilt Kalınlığı
+# Insulin: İnsülin.
+# BMI: Beden kitle indeksi.
+# DiabetesPedigreeFunction: Soyumuzdaki kişilere göre diyabet olma ihtimalimizi hesaplayan bir fonksiyon.
+# Age: Yaş (yıl)
+# Outcome: Kişinin diyabet olup olmadığı bilgisi. Hastalığa sahip (1) ya da değil (0)
+
+
+# 1. Exploratory Data Analysis
+# 2. Data Preprocessing
+# 3. Model & Prediction
+# 4. Model Evaluation
+# 5. Model Validation: Holdout
+# 6. Model Validation: 10-Fold Cross Validation
+# 7. Prediction for A New Observation
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
+from sklearn.model_selection import train_test_split, cross_validate
+
+diabets = pd.read_csv("./datasets/diabetes.csv")
+df = diabets.copy()
