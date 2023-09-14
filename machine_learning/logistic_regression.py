d552dc02ca606d41be9f60a84fabcb058f1f8c9c
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-09-14 21:26:47 +0300

    log-reg model done

--- machine_learning/logistic_regression.py
+++ machine_learning/logistic_regression.py
@@ -43,8 +43,132 @@ import seaborn as sns
 
 from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
+from sklearn.metrics import (
+    accuracy_score,
+    roc_auc_score,
+    confusion_matrix,
+    classification_report,
+    RocCurveDisplay,
+)
 from sklearn.model_selection import train_test_split, cross_validate
 
 diabets = pd.read_csv("./datasets/diabetes.csv")
 df = diabets.copy()
+
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[
+        (dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)
+    ].any(axis=None):
+        return True
+    else:
+        return False
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+df.head()
+df.shape
+
+
+######################################################
+# Target'ın Analizi
+######################################################
+df["Outcome"].value_counts()
+
+sns.countplot(x="Outcome", data=df)
+plt.show()
+
+100 * df["Outcome"].value_counts() / len(df)
+
+######################################################
+# Feature Analizi
+######################################################
+
+df.describe().T
+
+df["Glucose"].hist(bins=20)
+plt.xlabel("BloodPressure")
+plt.show()
+
+
+def plot_numerical_col(dataframe, numerical_col):
+    dataframe[numerical_col].hist(bins=20)
+    plt.xlabel(numerical_col)
+    plt.show(block=True)
+    
+for col in df.columns:
+    plot_numerical_col(df, col) 
+    
+cols = [col for col in df.columns if "Outcome" not in col]
+
+for col in cols:
+    plot_numerical_col(df, col)
+    
+######################################################
+# Target vs Feature
+######################################################
+df.groupby("Outcome").agg({"Pregnancies": "mean"})
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+    
+for col in cols:
+    target_summary_with_num(df, "Outcome", col)
+    
+######################################################
+# Data Preprocessing
+######################################################
+
+df.shape
+df.head()
+
+df.describe().T
+
+for col in cols:
+    print(col, check_outlier(df, col))
+    
+
+replace_with_thresholds(df, "Insulin")
+
+for col in cols:
+    df[col] = RobustScaler().fit_transform(df[[col]])
+
+df.head()
+
+######################################################
+# Model & Prediction
+######################################################
+
+y = df["Outcome"]
+
+X = df.drop(["Outcome"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+log_model.intercept_
+log_model.coef_
+
+y_pred = log_model.predict(X)
+
+y_pred[0:10]
+
+y[0:10]
