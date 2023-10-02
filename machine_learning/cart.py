8021df071b2ed61ca91d6898eb7036aa0fe9fbe8
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-10-02 18:23:19 +0300

    cart modelling done

--- machine_learning/cart.py
+++ machine_learning/cart.py
@@ -215,7 +215,7 @@ def plot_importance(model, features, num=len(X), save=False):
         plt.savefig("importances.png")
 
 
-plot_importance(cart_final, X, 10, save=True)
+plot_importance(cart_final, X, 10, save=0)
 
 
 ################################################
@@ -223,5 +223,191 @@ plot_importance(cart_final, X, 10, save=True)
 ################################################
 
 
+train_score, test_score = validation_curve(
+    cart_final,
+    X,
+    y,
+    param_name="max_depth",
+    param_range=range(1, 11),
+    scoring="roc_auc",
+    cv=10,
+)
+
+mean_train_score = np.mean(train_score, axis=1)
+mean_test_score = np.mean(test_score, axis=1)
 
 
+plt.plot(range(1, 11), mean_train_score, label="Training Score", color="b")
+
+plt.plot(range(1, 11), mean_test_score, label="Validation Score", color="g")
+
+plt.title("Validation Curve for CART")
+plt.xlabel("Number of max_depth")
+plt.ylabel("AUC")
+plt.tight_layout()
+plt.legend(loc="best")
+plt.show()
+
+
+def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
+    train_score, test_score = validation_curve(
+        model,
+        X=X,
+        y=y,
+        param_name=param_name,
+        param_range=param_range,
+        scoring=scoring,
+        cv=cv,
+    )
+
+    mean_train_score = np.mean(train_score, axis=1)
+    mean_test_score = np.mean(test_score, axis=1)
+
+    plt.plot(param_range, mean_train_score, label="Training Score", color="b")
+
+    plt.plot(param_range, mean_test_score, label="Validation Score", color="g")
+
+    plt.title(f"Validation Curve for {type(model).__name__}")
+    plt.xlabel(f"Number of {param_name}")
+    plt.ylabel(f"{scoring}")
+    plt.tight_layout()
+    plt.legend(loc="best")
+    plt.show(block=True)
+
+
+val_curve_params(cart_final, X, y, "max_depth", range(1, 11), scoring="f1")
+
+cart_val_params = [["max_depth", range(1, 11)], ["min_samples_split", range(2, 20)]]
+
+for i in range(len(cart_val_params)):
+    val_curve_params(cart_model, X, y, cart_val_params[i][0], cart_val_params[i][1])
+
+
+################################################
+# 8. Visualizing the Decision Tree
+################################################
+
+# conda install graphviz
+# import graphviz
+
+
+def tree_graph(model, col_names, file_name):
+    tree_str = export_graphviz(
+        model, feature_names=col_names, filled=True, out_file=None
+    )
+    graph = pydotplus.graph_from_dot_data(tree_str)
+    graph.write_png(file_name)
+
+
+tree_graph(model=cart_final, col_names=X.columns, file_name="cart_final.png")
+
+cart_final.get_params()
+
+
+################################################
+# 9. Extracting Decision Rules
+################################################
+
+tree_rules = export_text(cart_final, feature_names=list(X.columns))
+print(tree_rules)
+
+
+################################################
+# 10. Extracting Python Codes of Decision Rules
+################################################
+
+# sklearn '0.23.1' versiyonu ile yapılabilir.
+# pip install scikit-learn==0.23.1
+
+print(skompile(cart_final.predict).to("python/code"))
+
+# pip install sqlalchemy
+# Burada bir hata var bunu düzeltmek için bak 
+print(skompile(cart_final.predict).to("sqlalchemy/sqlite"))
+
+print(skompile(cart_final.predict).to("excel"))
+
+
+################################################
+# 11. Prediction using Python Codes
+################################################
+
+
+def predict_with_rules(x):
+    return ((((((0 if x[6] <= 0.671999990940094 else 1 if x[6] <= 0.6864999830722809 else
+        0) if x[0] <= 7.5 else 1) if x[5] <= 30.949999809265137 else ((1 if x[5
+        ] <= 32.45000076293945 else 1 if x[3] <= 10.5 else 0) if x[2] <= 53.0 else
+        ((0 if x[1] <= 111.5 else 0 if x[2] <= 72.0 else 1 if x[3] <= 31.0 else
+        0) if x[2] <= 82.5 else 1) if x[4] <= 36.5 else 0) if x[6] <=
+        0.5005000084638596 else (0 if x[1] <= 88.5 else (((0 if x[0] <= 1.0 else
+        1) if x[1] <= 98.5 else 1) if x[6] <= 0.9269999861717224 else 0) if x[1
+        ] <= 116.0 else 0 if x[4] <= 166.0 else 1) if x[2] <= 69.0 else ((0 if
+        x[2] <= 79.0 else 0 if x[1] <= 104.5 else 1) if x[3] <= 5.5 else 0) if
+        x[6] <= 1.098000019788742 else 1) if x[5] <= 45.39999961853027 else 0 if
+        x[7] <= 22.5 else 1) if x[7] <= 28.5 else (1 if x[5] <=
+        9.649999618530273 else 0) if x[5] <= 26.350000381469727 else (1 if x[1] <=
+        28.5 else ((0 if x[0] <= 11.5 else 1 if x[5] <= 31.25 else 0) if x[1] <=
+        94.5 else (1 if x[5] <= 36.19999885559082 else 0) if x[1] <= 97.5 else
+        0) if x[6] <= 0.7960000038146973 else 0 if x[0] <= 3.0 else (1 if x[6] <=
+        0.9614999890327454 else 0) if x[3] <= 20.0 else 1) if x[1] <= 99.5 else
+        ((1 if x[5] <= 27.649999618530273 else 0 if x[0] <= 5.5 else (((1 if x[
+        0] <= 7.0 else 0) if x[1] <= 103.5 else 0) if x[1] <= 118.5 else 1) if
+        x[0] <= 9.0 else 0) if x[6] <= 0.19999999552965164 else ((0 if x[5] <=
+        36.14999961853027 else 1) if x[1] <= 113.0 else 1) if x[0] <= 1.5 else
+        (1 if x[6] <= 0.3620000034570694 else 1 if x[5] <= 30.050000190734863 else
+        0) if x[2] <= 67.0 else (((0 if x[6] <= 0.2524999976158142 else 1) if x
+        [1] <= 120.0 else 1 if x[6] <= 0.23899999260902405 else 1 if x[7] <=
+        30.5 else 0) if x[2] <= 83.0 else 0) if x[5] <= 34.45000076293945 else
+        1 if x[1] <= 101.0 else 0 if x[5] <= 43.10000038146973 else 1) if x[6] <=
+        0.5609999895095825 else ((0 if x[7] <= 34.5 else 1 if x[5] <=
+        33.14999961853027 else 0) if x[4] <= 120.5 else (1 if x[3] <= 47.5 else
+        0) if x[4] <= 225.0 else 0) if x[0] <= 6.5 else 1) if x[1] <= 127.5 else
+        (((((1 if x[1] <= 129.5 else ((1 if x[6] <= 0.5444999933242798 else 0) if
+        x[2] <= 56.0 else 0) if x[2] <= 71.0 else 1) if x[2] <= 73.0 else 0) if
+        x[5] <= 28.149999618530273 else (1 if x[1] <= 135.0 else 0) if x[3] <=
+        21.0 else 1) if x[4] <= 132.5 else 0) if x[1] <= 145.5 else 0 if x[7] <=
+        25.5 else ((0 if x[1] <= 151.0 else 1) if x[5] <= 27.09999942779541 else
+        ((1 if x[0] <= 6.5 else 0) if x[6] <= 0.3974999934434891 else 0) if x[2
+        ] <= 82.0 else 0) if x[7] <= 61.0 else 0) if x[5] <= 29.949999809265137
+         else ((1 if x[2] <= 61.0 else (((((0 if x[6] <= 0.18299999833106995 else
+        1) if x[0] <= 0.5 else 1 if x[5] <= 32.45000076293945 else 0) if x[2] <=
+        73.0 else 0) if x[0] <= 4.5 else 1 if x[6] <= 0.6169999837875366 else 0
+        ) if x[6] <= 1.1414999961853027 else 1) if x[5] <= 41.79999923706055 else
+        1 if x[6] <= 0.37299999594688416 else 1 if x[1] <= 142.5 else 0) if x[7
+        ] <= 30.5 else (((1 if x[6] <= 0.13649999350309372 else 0 if x[5] <=
+        32.45000076293945 else 1 if x[5] <= 33.05000114440918 else (0 if x[6] <=
+        0.25599999725818634 else (0 if x[1] <= 130.5 else 1) if x[0] <= 8.5 else
+        0) if x[0] <= 13.5 else 1) if x[2] <= 92.0 else 1) if x[5] <=
+        45.54999923706055 else 1) if x[6] <= 0.4294999986886978 else (1 if x[5] <=
+        40.05000114440918 else 0 if x[5] <= 40.89999961853027 else 1) if x[4] <=
+        333.5 else 1 if x[2] <= 64.0 else 0) if x[1] <= 157.5 else ((((1 if x[7
+        ] <= 25.5 else 0 if x[4] <= 87.5 else 1 if x[5] <= 45.60000038146973 else
+        0) if x[7] <= 37.5 else 1 if x[7] <= 56.5 else 0 if x[6] <=
+        0.22100000083446503 else 1) if x[6] <= 0.28849999606609344 else 0) if x
+        [6] <= 0.3004999905824661 else 1 if x[7] <= 44.0 else (0 if x[7] <=
+        51.0 else 1 if x[6] <= 1.1565000414848328 else 0) if x[0] <= 6.5 else 1
+        ) if x[4] <= 629.5 else 1 if x[6] <= 0.4124999940395355 else 0)
+
+
+X.columns
+
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+
+predict_with_rules(x)
+
+x = [6, 148, 70, 35, 0, 30, 0.62, 50]
+
+predict_with_rules(x)
+
+
+################################################
+# 12. Saving and Loading Model
+################################################
+
+joblib.dump(cart_final, "cart_final.pkl")
+
+cart_model_from_disc = joblib.load("cart_final.pkl")
+
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+
+cart_model_from_disc.predict(pd.DataFrame(x).T)
